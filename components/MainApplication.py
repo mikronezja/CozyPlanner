cfbfda9f652a20a166b4dd75db26875ae69c4971
Julia Demitraszek <juliademi05@gmail.com>
2025-04-03 20:18:39 +0200

    okay now it works

--- components/MainApplication.py
+++ components/MainApplication.py
@@ -8,6 +8,6 @@ class MainApplication:
     def display(self): # ALWAYS REMEMBER TO UPDATE THE PAGE!
         self.page.bgcolor = "#a7dfff"
         self.page.add(Navbar(["Home","Eisenhower Matrix", "To-do list", "Pomodoro"], self.page.window.width).get_container())
-        self.page.views.clear()
+        #self.page.views.clear()
         self.page.update()
         
