58144772e1d45140b195c8a99caab2bd3ee2660f
Julia Demitraszek <juliademi05@gmail.com>
2025-06-13 14:48:26 +0200

    corrected dates

--- components/CalendarElements/DateBox.py
+++ components/CalendarElements/DateBox.py
@@ -4,8 +4,9 @@ import datetime
 class DayContainer(ft.Container):
     def __init__(self, day, month, year, on_click = None):
         super().__init__()
+        now = datetime.datetime.now()
 
-        self.text = ft.Text(str(day), size=20, color='#702106' if day != datetime.datetime.now().day else ft.Colors.BLUE)
+        self.text = ft.Text(str(day), size=20, color='#702106' if ( day != now.day or month != now.month or year != now.year )else ft.Colors.BLUE)
 
         self.content = ft.Column(
             [self.text],
